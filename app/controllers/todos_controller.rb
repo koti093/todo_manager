389b2f272f25041fc39ff6f807736aed14f24a0b
koti093 <koti093@gmail.com>
2021-11-29 20:27:59 +0530

    L8 Suggested functionality

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -14,14 +14,19 @@ class TodosController < ApplicationController
 
   def create
     todo_text = params[:todo_text]
-    due_date = DateTime.parse(params[:due_date])
-    new_todo = Todo.create!(
+    due_date = params[:due_date].to_s
+    puts due_date
+    new_todo = Todo.new(
       todo_text: todo_text,
       due_date: due_date,
       completed: false,
     )
-    response_text = "Hey your new todo is created with the id #{new_todo.id}"
-    render plain: response_text
+    if new_todo.save
+      redirect_to todos_path
+    else
+      flash[:error] = new_todo.errors.full_messages.join(", ")
+      redirect_to todos_path
+    end
   end
 
   def update
@@ -31,6 +36,11 @@ class TodosController < ApplicationController
     todo.completed = completed
     todo.save!
     redirect_to todos_path
-    #render plain: " updated todo completed status to #{completed}"
+  end
+  def destroy
+    id = params[:id]
+    todo = Todo.find(id)
+    todo.destroy
+    redirect_to todos_path
   end
 end
